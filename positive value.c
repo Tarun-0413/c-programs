a1290e09c77fc7b60521c1b711c547b9da97b514
Tarun-0413 <tarunduvvada13@gmail.com>
2026-04-22 15:46:00 +0530

    Add files via upload

---
+++ positive value.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+int a; 
+ printf("enter the A value\n");
+ scanf("%d",&a);
+ printf("a is +ve value i.e is: %d",a<0);
+}
