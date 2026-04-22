ce3682d613fa30ef9e3e8587a4295931491ddc32
Tarun-0413 <tarunduvvada13@gmail.com>
2026-04-22 15:40:00 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a=65;
+   int b=++a;
+   int c=((++a)+(b--));
+   int d=((a++)+(--b)+(c++));
+   printf("%d",(a+b+c+d));
+    return 0;
+}
